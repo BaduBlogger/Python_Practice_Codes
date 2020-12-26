f8c7843aa9fd9889cabfb755bbb9baf17a1fe42d
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2020-12-26 21:44:03 +0530

    Add files via upload
    Saving and Loading of Numpy Array

---
+++ Numpy_Practice/SavingNumpyArray.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+n1 = np.array([10,20,30,40,50])
+np.save('my_array',n1)		#for saving array, first parameter:"Userdefined name", second parameter:"array object"
+
+n2 = np.load('my_array.npy')	#for loading the array into new object
+print(n2)
