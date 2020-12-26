669b2c00d562f0dc731434efc76220002d89a6ee
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2020-12-26 21:38:07 +0530

    Add files via upload
    Numpy Maths Functions :
    a) Mean
    b) Median
    c) Standard Deviation

---
+++ Numpy_Practice/NumpyMathFunctions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+n1 = np.array([10,20,30,40,50,60,70,80,90,100])
+
+print('Mean of Array is :' + str(np.mean(n1)))
+
+print('Standard Deviation of Array is :' + str(np.std(n1)))
+
+print('Median of Array is :' + str(np.median(n1)))
