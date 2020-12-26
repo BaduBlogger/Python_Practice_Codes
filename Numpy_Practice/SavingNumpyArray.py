3980f5c789e4dab1b0e731a78f74a1df191a1c5b
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2020-12-26 21:47:45 +0530

    Add files via upload

--- Numpy_Practice/SavingNumpyArray.py
+++ Numpy_Practice/SavingNumpyArray.py
@@ -3,5 +3,5 @@ import numpy as np
 n1 = np.array([10,20,30,40,50])
 np.save('my_array',n1)		#for saving array, first parameter:"Userdefined name", second parameter:"array object"
 
-n2 = np.load('my_array.npy')	#for loading the array into new object
+n2 = np.load('my_array.npy')	#for loading the array into new object, .npy for numpy array
 print(n2)
