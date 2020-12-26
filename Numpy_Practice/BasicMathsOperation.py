be24ca4f73ee1872b563bd191028363a450eb61e
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2020-12-26 21:33:46 +0530

    Add files via upload
    Performing Basic Maths Operations on Array :
    a) Addition
    b) Substraction
    c) Division
    d) Multiplication

---
+++ Numpy_Practice/BasicMathsOperation.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+n1 = np.array([10,20,30,40,50])
+n1 = n1*2	#for Multiplication
+
+print('Multiplying array with 2 :')
+print(n1)
+
+n1 = n1+2	#for Addition
+
+print('Adding array with 2 :')
+print(n1)
+
+n1 = n1/2	#for Division
+
+print('Dividing array with 2 :')
+print(n1)
+
+n1 = n1 - 2	#for Subtraction
+print('Subtracting array with 2 :')
+print(n1)
