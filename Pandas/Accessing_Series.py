8b3308895ee82e3166b2b5f4758259c143118dfe
BaduBlogger <badu.bhadgaonkar@gmail.com>
2021-01-08 18:56:39 +0530

    Accessing Series objs

---
+++ Pandas/Accessing_Series.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+s1 = pd.Series([1,2,3,4,5,6,7,8,9])
+
+print(s1)
+
+print(s1[7])	#Here element at index 7 is displayed
+
+print(s1[:6])	#Here 0-5 are display and 6 is excluded
+
+print(s1[-3:])	#Here last 3 elements are displayed
