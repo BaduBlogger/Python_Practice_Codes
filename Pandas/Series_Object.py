fa750a011a4a6c51c6d26ffbe7f64d791eb316c2
BaduBlogger <badu.bhadgaonkar@gmail.com>
2021-01-08 17:43:14 +0530

    Series Object Example

---
+++ Pandas/Series_Object.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+list1 = [1,2,3,4,5,6]	#list of 6 elements
+s1 = pd.Series(list1)	#Here the series object if filled with list elements as per their index from 0 and data type is also printed
+print(s1)
