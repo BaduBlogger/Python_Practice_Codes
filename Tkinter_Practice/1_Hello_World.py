519de6dd6e58bb6c77189463a12352538266cff3
BaduBlogger <badu.bhadgaonkar@gmail.com>
2021-01-13 22:52:14 +0530

    First code

---
+++ Tkinter_Practice/1_Hello_World.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter.ttk import *
+
+#Creating Main window named root
+root = Tk()
+root.title("First Tkinter Program")	#Title of Frame, displayed on left top of the window
+
+label = Label(root,text="Hello World").pack()
+
+root.mainloop()	#Calling mainloop function. Tells application is ready and tells code to keep displaying 
