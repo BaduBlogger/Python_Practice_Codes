161709ca9e9c96b2d2e7605a4e45e3a4c8a61385
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2021-03-20 16:24:35 +0530

    Update 1_Hello_World.py

--- Tkinter_Practice/1_Hello_World.py
+++ Tkinter_Practice/1_Hello_World.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter.ttk import *
 
 #Creating Main window named root
-root = Tk()
+root = Tk()  #constructor calling the top level widget of tkinter and storing into the obj root
 root.title("First Tkinter Program")	#Title of Frame, displayed on left top of the window
 
 label = Label(root,text="Hello World").pack()
