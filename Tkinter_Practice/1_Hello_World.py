ac97741b3fdb2d0170ca3dab01448200f48f6f1a
BaduBlogger <76211431+BaduBlogger@users.noreply.github.com>
2021-03-20 21:46:13 +0530

    Update 1_Hello_World.py

--- Tkinter_Practice/1_Hello_World.py
+++ Tkinter_Practice/1_Hello_World.py
@@ -5,7 +5,7 @@ from tkinter.ttk import *
 root = Tk()  #constructor calling the top level widget of tkinter and storing into the obj root
 root.title("First Tkinter Program")	#Title of Frame, displayed on left top of the window
 
-label = Label(root,text="Hello World").pack()
+label = Label(root,text="Hello World").pack() # pack is a geometric management method to put it on the window
 
 root.mainloop()	#Calling mainloop function. Tells application is ready and tells code to keep displaying 
 
